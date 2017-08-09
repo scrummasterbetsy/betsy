7e6ccd80e29a4d68cdd2bc5d5056d6b777864893
scrummasterbetsy <30596348+scrummasterbetsy@users.noreply.github.com>
2017-08-09 12:17:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -113,9 +113,7 @@ app.post('/', function (req, res) {
 			} // end if
 			console.log('Modify status code: '+response.statusCode); 
 			if (!error && response.statusCode >= 200 && response.statusCode <=299) { // all 2xx codes are OK
-			   console.log(body);
-			   let strJSON = JSON.parse(body);
-			   console.log(strJSON);
+			   // NO BODY console.log(body);
 			   assistant.ask('Issue '+strProjectID+'-'+strIssueID+' successfully changed from '+strStatusCur+' to '+strStatusTarget+'. '+nextPrompt);
 		   	} else {
 			  assistant.ask('There was an error in the execution of ChangeIssueStatus.'+nextPrompt);
