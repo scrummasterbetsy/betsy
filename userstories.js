af29dbe552564556812f274edf4e54b1410fb912
scrummasterbetsy <30596348+scrummasterbetsy@users.noreply.github.com>
2017-08-09 14:10:55 -0400

    Update userstories.js

--- userstories.js
+++ userstories.js
@@ -1,5 +1,5 @@
  function UserStories(assistant) {
    console.log('UserStories');
    let nextPrompt = Prompts[Math.floor(Math.random() * Prompts.length)];
-   assistant.ask('This is the user story intent. '+nextPrompt);
+   assistant.ask('This is the new user story intent. '+nextPrompt);
 }
