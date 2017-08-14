b2f788ed3aa3c0d74e7e5b34b22bbd3a5ed567a2
scrummasterbetsy <30596348+scrummasterbetsy@users.noreply.github.com>
2017-08-14 15:08:10 -0400

    Update setproject.js

--- setproject.js
+++ setproject.js
@@ -23,6 +23,7 @@ exports.SetProject = function(assistant) {
     } // end if
     console.log(response.statusCode); 
     if (!error && response.statusCode == 200) {
+	 console.log(body);
         let strJSON = JSON.parse(body);
         //console.log(strJSON);
         
