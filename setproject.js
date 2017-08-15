0a9a2e88bccb63b6981def72f39ea53f5078bb1c
Michael Kolb <Michael_Kolb@cable.comcast.com>
2017-08-14 22:01:50 -0400

    Refactored SetProject function to use Node jira-client.

--- setproject.js
+++ setproject.js
@@ -1,50 +1,34 @@
-let request = require('request');
+let jira = require('./app/util/jira').jira;
+const util = require('util');
 
 exports.SetProject = function(assistant) {
-   console.log('+++SetProject+++');
    //console.log(assistant.getRawInput());
-   let strProjectID = assistant.getArgument('ProjectID');
-   console.log('ProjectID '+strProjectID); 
-   
-  
-   // Configure the request
-  let options = {
-    headers: {'Content-Type':'application/json', 'Authorization':'Basic YmV0c3k6QmV0c3lCb3Q4MjI='},
-    method: 'GET',
-    url:  'https://projectbetsy.atlassian.net/rest/api/2/project'
-  }
+   let strProjectID = assistant.getArgument('ProjectID').toUpperCase();
+   console.log('+++SetProject ProjectID=' + strProjectID + ' +++');
 
-  // Start the request
-  request(options, function (error, response, body) {
-    if (error) {
-      console.log(error);
-        assistant.ask('There was an error in Set Project. '+error +nextPrompt);
-        return;
-    } // end if
-    console.log(response.statusCode); 
-    if (!error && response.statusCode == 200) {
-        let strJSON = JSON.parse(body);
-        //console.log(strJSON);
-        
-        // Check if current project exists
-        for (var key in strJSON) {
-          if (strProjectID.toUpperCase() == strJSON[key].key.toUpperCase()) {
-            assistant.ask('\nOK. The project is now set to "'+strProjectID+'". To change the project just say "Set Project". Please tell me what to do.');
-            return;
-          } // end 
-        } // end for
-         
-         // Project not found. Hence we need to ask one more time.
-        // Prepare output 
-        let strOut = 'I could not find project "'+strProjectID+'". \nHere is the list of all available projects: ';
-	      for (var key in strJSON) {
-           strOut += ' \n'+strJSON[key].key+'.';
-        } // end for     
-        strOut += ' \nWhich one do you want to work with?';
+   jira.listProjects()
+    .then(function (body) {
+      // Check if current project exists
+      body.forEach(function (item) {
+        if (strProjectID == item.key.toUpperCase()) {
+          assistant.ask('OK. The project is now set to "' + item.key + '". To change the project just say "Set Project". Please tell me what to do.');
+          return;
+        }
+      });
 
-        //console.log(strOut);
-        assistant.setContext('AskForProjectID',1);
-        assistant.ask(strOut);
-    } // end if (!error && response.statusCode == 200)
-}) // end request 
+      // Project not found. Hence we need to ask one more time.
+      var strOut = 'I could not find project "'+strProjectID+'". \nHere is the list of all available projects: ';
+      body.forEach(function (item) {
+        strOut += ' \n'+item.key+'.';
+      })
+      strOut += ' \nWhich one do you want to work with?';
+
+      assistant.setContext('AskForProjectID',1);
+      assistant.ask(strOut);
+
+    }).catch(function (err) {
+      console.error(err);
+      assistant.ask('There was an error in Set Project. '+error +nextPrompt);
+      return;
+    });
 } // end SetProject
